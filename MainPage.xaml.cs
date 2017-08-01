eb1a0050aa3d189ad6535f48ea3ecf991ff1f9d5
BJRambo <sosifam@070805.co.kr>
2017-08-01 21:03:52 +0900

    deleted comment

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,16 +13,10 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// 빈 페이지 항목 템플릿에 대한 설명은 https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x412에 나와 있습니다.
-
 namespace Login_form
 {
-    /// <summary>
-    /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
-
         public MainPage()
         {
             this.InitializeComponent();
